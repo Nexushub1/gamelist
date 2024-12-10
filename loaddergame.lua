558c8032e718b0e6c38834dbbae83fe83071cb83
Nexushub1 <testbot.qzc1@gmail.com>
2024-12-10 20:33:44 +0700

    Update loaddergame.lua

--- loaddergame.lua
+++ loaddergame.lua
@@ -1,3 +1,3 @@
 getgenv().NameGame = {
-  [16732694052] = "https://raw.githubusercontent.com/AhmadV99/Script-Games/main/Fisch.lua", -- Fisch
+  [16732694052] = "https://raw.githubusercontent.com/Nexushub1/gamelist/refs/heads/main/fisch.lua", -- Fisch
 }
